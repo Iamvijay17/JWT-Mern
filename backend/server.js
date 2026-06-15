ea56d42d4d04dff5d1f074a844438abab42578b2
iamvijay17 <vijay9578979255@gmail.com>
2026-06-15 20:03:34 +0530

    update db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = 3000;
 
 app.listen(3000, () => {
+  connectDB();
   console.log(`🚀 Server Running On Port ${PORT}`);
 });
---
+++ backend/src/config/env.js
@@ -0,0 +1,2 @@
+export const DB_URI =
+  "mongodb+srv://clevermerkle2:admin@curddb.r9ufyvy.mongodb.net/user-management?appName=curdDB";
