9a457a9731b09a4ef90b403c0ecbd0cb3434ccac
iamvijay17 <vijay9578979255@gmail.com>
2026-06-18 09:02:07 +0530

    add User model with schema for registration

---
+++ backend/src/modules/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    age: {
+      type: Number,
+      required: [true, "Age is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
