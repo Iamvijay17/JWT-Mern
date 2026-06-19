81bd1279bc26e709e23b248b628b47849b8e7b9b
Iamvijay17 <vijay9578979255@gmail.com>
2026-06-19 21:11:36 +0530

    feat: implement form handling with react-hook-form and update styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
 function App() {
-  return <>
-  <div className=""></div>
-  </>;
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { error },
+    ...props
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data, props);
+  };
+
+  console.log(watch("example"));
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name", { required: true, maxLength: 20 })} />
+          <input {...register("age", { required: true })} />
+          <input {...register("email")} />
+          <input {...register("password")} />
+          <input type="submit" />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
